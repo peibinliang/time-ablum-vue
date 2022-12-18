684e873ebf5e193f1c5467fa8bfa478d62f3057d
peibinliang <1805993643@qq.com>
2022-12-18 15:31:41 +0800

    整体布局优化

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,6 @@
 
 <style>
 #app{
-  height: 100%;
+  min-height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import './assets/gloable.css';
+import './assets/gloable.css'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI,{size:"small"});
+Vue.use(ElementUI, { size: "mini" });
 
 new Vue({
   router,
